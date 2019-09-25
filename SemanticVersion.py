884391004c8bc1908195dc06d3d81f20038dc440
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 16:00:41 +0900

    コード成形

--- SemanticVersion.py
+++ SemanticVersion.py
@@ -11,10 +11,6 @@ class SemanticVersion:
             return True
         return False
 
-    def __str__(self):
-         self.version = f'{self.major}.{self.minor}.{self.patch}'
-         return self.version
-
 
 def main():
     py370 = SemanticVersion(3, 7, 0)
