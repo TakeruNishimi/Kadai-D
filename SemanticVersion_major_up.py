b84a862748b64cec17b3a065c879051d297f60d2
TakeruNishimi <takechu.nishimi@gmail.com>
2019-09-25 16:21:29 +0900

    メジャーアップデート用の関数を作成

---
+++ SemanticVersion_major_up.py
@@ -0,0 +1,48 @@
+class SemanticVersion:
+    def __init__(self, major, minor, patch):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __eq__(self, other):
+        if not isinstance(other, SemanticVersion):
+            NotImplemented
+        if (self.major == other.major) and (self.minor == other.minor) and (self.patch == other.patch):
+            return True
+        return False
+
+    def __str__(self):
+        self.version = f'{self.major}.{self.minor}.{self.patch}'
+        return self.version
+
+    def patch_version_up(self):
+        self.patch += 1
+        return SemanticVersion(self.major, self.minor, self.patch)
+
+    def minor_version_up(self):
+        self.minor += 1
+        self.patch = 0
+        return SemanticVersion(self.major, self.minor, self.patch)
+
+    def major_version_up(self):
+        self.major += 1
+        self.minor = 0
+        self.patch = 0
+        return SemanticVersion(self.major, self.minor, self.patch)
+
+
+def main():
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    py371 = py370.patch_version_up()
+    print(SemanticVersion(3, 7, 1) == py371)  # True
+
+    py380 = py371.minor_version_up()
+    print(SemanticVersion(3, 8, 0) == py380)
+
+    py400 = py371.major_version_up()
+    print(SemanticVersion(4, 0, 0) == py400)
+
+
+if __name__ == '__main__':
+    main()
